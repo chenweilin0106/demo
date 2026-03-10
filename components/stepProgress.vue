03c2e36ff06b7b08e73e634e8378ba0dcc6d7bc6
chenweilin <wbp-037q7gmp1@dingtalk.com>
2026-03-10 10:56:27 +0800

    进度条组件更新

--- components/stepProgress.vue
+++ components/stepProgress.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="compoPsBg stepProgress">
+  <div class="compoPsBg">
     <div class="psBg">
-      <div class="currentPs" :style="`backgroundSize: ${psPercent}% 100%;`"></div>
-      <div class="nodeItem" :style="`width: ${100 / tot_list.length}%`" v-for="item in tot_list" :key="item.id">
-        <div :class="['itemNodeIcon', item.has_right != 0 ? 'active' : null]"></div>
-        <div class="flex align-center justify-center" :class="['rewardDiv', `status${item.has_right}`]" @click="nodeIconCk(item)">
-          <div class="iconDiv flex-column position-relative">
-            <lazy-component class="icon flex align-center justify-center position-relative">
-              <PublicImg :imgName="item.award.icon" :imgType="item.award.type" />
-            </lazy-component>
-            <div class="text flex-1 flex align-center justify-center line-height-1">{{ $toShowText(item.award) }}</div>
-            <div v-if="item.show_price" class="priceLabel radius-999 flex align-center position-absolute line-height-1">{{ item.show_price }}<img :src="IconPath('zs_42_32.png')" /></div>
+      <div class="currentPs" :style="`width: calc(${psPercent}%);`"></div>
+      <div class="nodeItem" :style="`width: ${100/tot_list.length}%`" v-for="(item) in tot_list" :key="item.id">
+        <div :class="['itemNodeIcon', item.has_right!=0?'active':null]"></div>
+        <div :class="['rewardDiv',`status${item.has_right}`]" @click="nodeIconCk(item)">
+          <div class="rewardIcon">
+            <PublicImg :imgName="item.icon" :imgType="item.type"></PublicImg>
+            <div class="previewMark" v-if="item.key==20000"></div>
           </div>
-          <div class="mileShow">{{ item.need_num }}</div>
+          <div class="rewardText">{{$toShowText(item, { pst_privilege: null, ring: null })}}</div>
+          <div class="mileShow"><strong>累计收礼达</strong><br>{{item.key}}</div>
           <div class="redMark" v-if="item.has_right == 1"></div>
-          <!-- <div v-if="index==5" class="topMark line-height-1 flex align-center">属性称号</div> -->
+          <!-- <div v-if="typeToNameMap[item.type]" class="typeLabel line-height-1 flex align-center justify-center position-absolute">{{typeToNameMap[item.type]}}</div> -->
+          <div v-if="item.key==3000" class="typeLabel line-height-1 flex align-center justify-center position-absolute">礼物赠送权</div>
+          <div v-if="item.key==3000||item.key==15000" class="priceLabel radius-999 flex align-center position-absolute line-height-1">
+            {{ item.key == 3000 ? 520 : 3344 }}<img :src="IconPath('zs_42_32.png')" alt="" />
+          </div>
+          <!-- <div class="newMark" v-if="item.key==20000">新</div> -->
         </div>
       </div>
     </div>
@@ -22,12 +25,12 @@
 </template>
 
 <script>
-import { receiveValueApi } from '@/api'
+import { mapState } from 'vuex'
+import { getPageData } from '@/api/index'
 
 export default {
   name: 'stepProgress',
   props: ['tot_num', 'tot_list'],
-  components: {},
   data() {
     return {
       psPercent: 0, // 进度条百分比
@@ -36,15 +39,15 @@ export default {
   },
   created() {
     this.psPercent = this.calcPsPercent()
-    // console.log(this.psPercent)
   },
-  mounted() {},
-  computed: {},
   watch: {
     tot_num() {
       this.psPercent = this.calcPsPercent()
       this.scrollHas()
     }
+  },
+  computed: {
+    ...mapState(['typeToNameMap'])
   },
   methods: {
     /**
@@ -52,26 +55,27 @@ export default {
      */
     nodeIconCk(item) {
       console.log(item)
+      // this.$emit('openReceivePopup', [{ icon: 'shlm_120_120.png', id: 34, max_nums: 10000, name: '山花烂漫', nums: 5, text: '5天', type: 'chat_bubble_pub' }])
       if (item.has_right == 1) {
-        receiveValueApi(item.id).then((res) => {
-          if (res.errno == 0) {
-            // 打开获奖弹窗
-            this.$emit('openReceivePopup', res, true)
-            this.scrollHas()
-          } else {
-            this.$toast(res.errmsg)
-          }
+        getPageData({ type: 'present_contact_rev', mark: item.key }).then((res) => {
+          if (res.errno) return this.$toast(res.errmsg)
+          // 打开获奖弹窗
+          this.$emit('openReceivePopup', [res.data.award], item.key)
+          this.scrollHas()
         })
+      } else {
+        if (item.key == 20000) {
+          this.$emit('previewImgCk', item)
+        }
       }
     },
     /**
      * 滚动到当前可领取项
      */
     scrollHas() {
-      const man = document.getElementsByClassName('compoPsBg')[0]
-      // console.log(document.getElementsByClassName('nodeItem')[0].clientWidth)
+      let man = document.getElementsByClassName('compoPsBg')[0]
       let scrollIndex = 0
-      for (const index in this.tot_list) {
+      for (let index in this.tot_list) {
         if (this.tot_list[index].has_right == 2) {
           scrollIndex = index
         }
@@ -80,9 +84,7 @@ export default {
           break
         }
       }
-      // console.log(scrollIndex)
-      const len = document.getElementsByClassName('nodeItem')[0].clientWidth * (scrollIndex - 1)
-      // console.log(len)
+      let len = document.getElementsByClassName('nodeItem')[0].clientWidth * (scrollIndex - 1)
       man.scrollTo({
         left: len,
         behavior: 'smooth'
@@ -92,32 +94,31 @@ export default {
      * 计算进度条百分比
      */
     calcPsPercent() {
-      if (this.tot_num >= this.tot_list[this.tot_list.length - 1].need_num) {
+      if (this.tot_num >= this.tot_list[this.tot_list.length - 1].key) {
         return 100
       }
       let node = 0 // 处于第几个节点
-      for (const index in this.tot_list) {
-        if (this.tot_list[index].need_num >= this.tot_num) {
+      for (let index in this.tot_list) {
+        if (this.tot_list[index].key >= this.tot_num) {
           node = index
           break
         }
       }
-      // console.log('node', node)
       if (node > 0) {
-        const a = this.tot_num - this.tot_list[node - 1].need_num
-        const b = this.tot_list[node].need_num - this.tot_list[node - 1].need_num
-        const modePs = a / b // 于当前节点进度
-        const eachNodePs = 100 / this.tot_list.length // 每个节点的占比
-        const x = eachNodePs * Number(node) - eachNodePs / 2 // 已完成的节点的百分比
-        const y = this.nodePercent // 节点长度百分比的一半
-        const z = modePs * (eachNodePs - this.nodePercent * 2) // 占当前节点进度条长的百分比
+        let a = this.tot_num - this.tot_list[node - 1].key
+        let b = this.tot_list[node].key - this.tot_list[node - 1].key
+        let modePs = a / b // 于当前节点进度
+        let eachNodePs = 100 / this.tot_list.length // 每个节点的占比
+        let x = (eachNodePs * Number(node)) - (eachNodePs / 2) // 已完成的节点的百分比
+        let y = this.nodePercent // 节点长度百分比的一半
+        let z = modePs * (eachNodePs - (this.nodePercent * 2)) // 占当前节点进度条长的百分比
         return x + (node > 0 ? y : 0) + z
       } else {
-        if (this.tot_list[node].need_num == 0) {
+        if (this.tot_list[node].key == 0) {
           return 0
         } else {
-          const e = 100 / this.tot_list.length / 2 - this.nodePercent
-          return (this.tot_num / this.tot_list[node].need_num) * e
+          let e = (100 / this.tot_list.length) / 2 - this.nodePercent
+          return this.tot_num / this.tot_list[node].key * e
         }
       }
     }
@@ -126,81 +127,73 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.compoPsBg {
-  width: 632px;
-  height: 360px;
-  margin: 0 auto;
+.compoPsBg{
+  width: 660px; // 进度条宽度
+  height: 326px; // 进度条高度 + 奖励高度 + 底部文字高度 + 上下边距高度
   overflow-y: hidden;
-  overflow-x: scroll;
+  // overflow-x: scroll;
+  overflow-X: hidden;
   overscroll-behavior: none;
-  // padding-left: 30px;
-  .psBg {
-    width: 632px;
+  margin: 0 auto 0 auto;
+  .psBg{
+    width: 100%;
     height: 14px;
-    background: #dfccff;
-    // background: linear-gradient(to left, transparent,transparent 6.25%,#F6CE79 6.25%,#F6CE79 93.75%, transparent 93.75%, transparent);
-    border-radius: 999999999px;
-    // border: 4px solid #5890F5;
+    //background: linear-gradient(to right,#FDBFD8 87.5%, transparent 87.5%, transparent);
+    background: linear-gradient(to right,#FFC5DE);
+    border-radius: 25px;
     position: relative;
-    top: 279px;
+    top: 204px;
     display: flex;
-    &::after {
+    &::after{
       content: '';
-      height: 6px;
-      background: #5f53be;
-      border-radius: 999999999px;
-      // background: linear-gradient(to right, transparent,transparent 6.25%,#C47E34 6.25%,#C47E34 93.75%, transparent 93.75%, transparent);
+      width: calc(100% - 8px);
+      height: calc(100% - 8px);
+      //background: linear-gradient(to right,#E26996 87.5%, transparent 87.5%, transparent);
+      background: linear-gradient(to right,#FF7CB5);
+      border-radius: 25px - 4;
       position: absolute;
-      left: 4px;
-      right: 4px;
+      left: 50%;
       top: 50%;
-      transform: translateY(-50%);
+      transform: translate(-50%, -50%);
       z-index: 1;
     }
-    // &::before{
-    //   content: '';
-    //   width: 30px;
-    //   height: 100%;
-    //   position: absolute;
-    //   right: -30px;
-    //   top: 0;
-    // }
-    .currentPs {
-      width: 632px;
-      height: 14px;
-      background: linear-gradient(#ffdf4c, #ffdf4c) no-repeat;
-      // background-image: linear-gradient(to right, transparent 0px, transparent 81.25px, #FFAF14 81.25px, #FFAF14 1218px, transparent 1218px, transparent 1300px);
+    .currentPs{
+      width: 100%;
+      height: 100%;
+      //background-image: linear-gradient(to right, #8FBEFF 577.5px, transparent 577.5px, transparent 660px);
+      background-image: linear-gradient(to right, #8FBEFF);
       border: none;
-      border-radius: 999999999px;
+      border-radius: 25px - 4;
       position: absolute;
-      left: 0px;
-      top: 0;
+      left: 0;
+      top: 50%;
+      transform: translate(0, -50%);
       z-index: 2;
     }
-    .nodeItem {
+    .nodeItem{
       // width: 25%;
-      height: 14px;
+      height: 100%;
       position: relative;
       z-index: 3;
-      .itemNodeIcon {
-        width: 42px;
-        height: 42px;
-        background: #5f53be;
+      .itemNodeIcon{
+        width: 40px;
+        height: 40px;
+        background: #FF7CB5;
         border-radius: 50%;
-        border: 4px solid #dfccff;
+        border: 4px solid #FFC5DE;
         position: absolute;
         left: 50%;
         top: 50%;
         transform: translate(-50%, -50%);
-        &.active {
-          background: #ffdf4c;
+        &.active{
+          background: #FFAF14;
           border: 0px;
           border-radius: 50%;
-          &::after {
+          &::after{
             content: '';
             width: 50px;
             height: 59px;
-            background: url('@/assets/mk1_27.png') no-repeat left top/100% 100%;
+            background: url('@/pages/gardenParty/assets/mk3_4.png') no-repeat left top/100% 100%;
             position: absolute;
             left: 50%;
             top: 60%;
@@ -208,137 +201,130 @@ export default {
           }
         }
       }
-      .rewardDiv {
-        width: 181px;
-        height: 233px;
-        background: url('@/assets/mk1_24.png') no-repeat left top/100% 100%;
+      .rewardDiv{
+        width: 144px;
+        height: 159px;
+        background: url('@/pages/gardenParty/assets/mk3_1.png') no-repeat left top/100% 100%;
         position: absolute;
         left: 50%;
-        transform: translateX(-50%);
-        top: -255px;
-        &.status1::after {
+        top: -18px;
+        transform: translateX(-50%) translateY(-100%);
+        &.status1::after{
           content: '';
-          width: 209px;
-          height: 264px;
-          background: url('@/assets/mk1_25.png') no-repeat left top/100% 100%;
+          width: 144px;
+          height: 159px;
+          background: url('@/pages/gardenParty/assets/mk3_3.png') no-repeat left top/100% 100%;
           position: absolute;
-          left: -16px;
-          top: -14px;
+          left: 0;
+          top: 0;
           z-index: 1;
         }
-        &.status2::after {
+        &.status2::after{
           content: '';
-          width: 181px;
-          height: 233px;
-          background: url('@/assets/mk1_26.png') no-repeat left top/100% 100%;
+          width: 144px;
+          height: 159px;
+          background: url('@/pages/gardenParty/assets/mk3_2.png') no-repeat left top/100% 100%;
           position: absolute;
-          left: 0px;
-          top: 0px;
-          z-index: 3;
+          left: 0;
+          top: 0;
+          z-index: 1;
         }
-        .iconDiv {
-          $borderWidth: 4px; /*边框粗细*/
-          $size: 120px; /*尺寸*/
-          $radius: 12px; /*圆角*/
-          $borderColor: #fff4ab; /*边框颜色*/
-          $iconBgc: #5e489f; /*图标背景色*/
-          $textBgc: #ae79ef; /*文本背景色*/
-          $iconHeight: 106px; /*图标高度*/
-          $fontSize: 22px; /*文本字体大小*/
-          $color: #ffffff; /*文本字体颜色*/
-          width: $size + $borderWidth * 2; /*宽度 蓝湖上选区不包括边框 所以加上边框宽度*/
-          height: 140px + $borderWidth * 2; /*高度 蓝湖上选区不包括边框 所以加上边框宽度*/
-          background-color: $borderColor; /*边框颜色*/
-          border-radius: $radius + $borderWidth; /*蓝湖上选区不包括边框 所以加上边框宽度*/
-          padding: $borderWidth;
-          .icon {
-            height: $iconHeight;
-            background: $iconBgc;
-            border-radius: $radius $radius 0 0;
+        .rewardIcon{
+          width: 90px;
+          height: 90px;
+          position: absolute;
+          left: 50%;
+          transform: translateX(-50%);
+          top: 15px;
+          &.titleSp{
+            width: 120px;
           }
-          .text {
-            color: $color;
-            font-size: $fontSize;
-            background-color: $textBgc;
-            border-radius: 0 0 $radius $radius;
-          }
-          .priceLabel {
-            z-index: 2;
-            font-size: 20px;
-            right: -6px;
-            top: 0;
-            color: #ffffff;
-            transform: translateY(-50%);
-            padding: 7px 5px 7px 10px;
-            background: linear-gradient(-90deg, #75b9ff, #ff84f8);
-            img {
-              width: 27px;
-              height: 20px;
-              margin: 0 0 0 2px;
-            }
+          .previewMark{
+            width: 122px;
+            height: 24px;
+            background: url('@/pages/gardenParty/assets/mk3_5.png') no-repeat left top/100% 100%;
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            bottom: 0;
           }
         }
-        .rewardText {
+        .rewardText{
           white-space: nowrap;
           font-size: 22px;
-          text-align: center;
           position: absolute;
           left: 50%;
           transform: translateX(-50%);
-          top: 100px;
-        }
-        .mileShow {
-          white-space: nowrap;
-          font-weight: 500;
-          font-size: 26px;
-          color: #ffffff;
-          text-align: center;
-          position: absolute;
-          left: 50%;
-          transform: translateX(-50%) translateY(100%);
-          bottom: -64px;
+          top: 110px;
           line-height: 1;
         }
-        .redMark {
-          width: 16px;
-          height: 16px;
-          background: #e22a2a;
-          border-radius: 50%;
-          position: absolute;
-          right: 15px;
-          top: 16px;
-          z-index: 10;
-        }
-        .topMark {
-          background: linear-gradient(-90deg, #ea8baf, #ea8baf);
-          border-radius: 17px;
-          font-size: 20px;
-          color: #ffffff;
-          font-weight: bold;
-          height: 28px;
+        .mileShow{
+          white-space: nowrap;
+          font-size: 24px;
+          color: #861F52;
+          text-align: center;
           position: absolute;
           left: 50%;
           transform: translateX(-50%);
-          padding: 0 6px;
-          top: -7px;
-          z-index: 2;
-          img {
-            width: 42px * 0.7;
-            height: 32px * 0.7;
-            vertical-align: middle;
+          top: 217px;
+          line-height: 28px;
+          strong{
+            font-weight: bold;
           }
         }
-        .topMarkSp {
-          background: linear-gradient(-90deg, #fff);
-          border-radius: 17px;
-          font-size: 20px;
-          color: #d48e45;
-          padding: 5px 10px;
+        .redMark{
+          width: 16px;
+          height: 16px;
+          background: #E22A2A;
+          border-radius: 50%;
           position: absolute;
+          right: 5px;
+          top: 32px;
+          z-index: 2;
+        }
+        .typeLabel{
+          z-index: 1;
           left: 50%;
           transform: translateX(-50%);
-          top: -15px;
-          z-index: 2;
+          top: 76px;
+          width: 102px;
+          height: 28px;
+          background: linear-gradient(90deg, transparent, #FFA5DD 16%, #9BC8FF 84%, transparent);
+          font-weight: 500;
+          font-size: 18px;
+          color: #FFFFFF;
+        }
+        .priceLabel {
+          font-size: 20px;
+          font-weight: 500;
+          right: -4px;
+          top: -4px;
+          color: #FFFFFF;
+          //transform: translateY(-50%);
+          padding: 4px 5px 4px 10px;
+          height: 34px;
+          background: linear-gradient(-90deg, #75B9FF, #FF84F8);
+          img {
+            width: 27px;
+            height: 20px;
+            margin: 0 0 0 2px;
+          }
+        }
+        .newMark{
+          width: 44px;
+          height: 36px;
+          background: linear-gradient(-90deg, #FF7FA6, #FD5AB6);
+          border-radius: 18px 19px 0px 18px;
+          position: absolute;
+          left: -2px;
+          top: -18px;
+          font-weight: 500;
+          font-size: 22px;
+          color: #FFFFFF;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          z-index: 3;
         }
       }
     }
