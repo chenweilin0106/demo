384b615ab3d23ef1c4ff29173eb06e43d03dee2f
chenweilin0106 <tr13535155jin@163.com>
2026-04-15 11:28:48 +0800

    Update api.js

--- components/js/api.js
+++ components/js/api.js
@@ -1,7 +1,9 @@
 import request from '@/utils/request'
 
 /**
- * 获取用户信息
+ * 获取用户信息（注意 request.js 中的 uid 覆盖）
+ *
+ * @param {string | number} [uid] 用户id
  * @returns {Promise} 用户信息
  * @example
  * {
@@ -9,10 +11,14 @@ import request from '@/utils/request'
  *   username: "用户名",
  *   profile_image: "头像地址",
  *   gender: "0" "0"-男 "1"-女
+ *   server_id: "服务器id" 1-倾国倾城 2-情深意长 3-天下无双 4-江山如画
+ *   server_name: "服务器名称"
+ *   code: "靓号"
+ *   pretty_type: "靓号类型" 1-经典/名流 2-典藏 3-至尊 4-吉日 5-情缘
  * }
  */
-export const userInfo = () => {
-  return request({ url: '/h5/user-info' })
+export const userInfo = (uid) => {
+  return request({ url: '/h5/user-info', data: { uid } })
 }
 
 /**
@@ -27,4 +33,11 @@ export const nowEquip = (data) => {
   let url = '/h5/equip'
   if (data.type == 'world_msg_skins') url = '/api/v5/h5/equip'
   return request({ url, data: { ...data } })
+}
+
+/**
+ * 获取用户资产信息
+ */
+export const propertyInfo = () => {
+  return request({ url: '/actserver/template2/receive', data: { type: 'comm_user_property' } })
 }
