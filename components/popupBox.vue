9d186f3fa39786192407ceb5757d174562a3878a
chenweilin0106 <tr13535155jin@163.com>
2026-05-15 13:57:40 +0800

    Update popupBox.vue

--- components/popupBox.vue
+++ components/popupBox.vue
@@ -2,14 +2,15 @@
   <van-popup v-model="isShowPo" :close-on-click-overlay="false" transition-appear get-container="body" @click-overlay="clickClose">
     <template v-if="isLight">
       <div class="topLight"></div>
-      <div class="bottomLight"></div>
     </template>
     <div class="popupContent" :class="`popupSize-${size}`">
       <template v-if="hasCostume">
-        <img :src="IconPath('tk_3.png')" class="leftIcon pointer-none position-absolute" />
-        <img :src="IconPath('tk_3_1.png')" class="rightIcon pointer-none position-absolute" />
+        <div class="leftIcon pointer-none position-absolute"></div>
+        <div class="rightIcon pointer-none position-absolute"></div>
       </template>
-      <img v-if="title" :src="IconPath(title)" class="titleIcon" />
+      <div v-if="title" class="titleIcon pointer-none position-absolute">
+        <div class="titleText"><div>{{title}}</div><div>{{title}}</div></div>
+      </div>
       <slot name="topSlot"></slot>
       <div class="main overflow-x-hidden overflow-y-scroll">
         <slot></slot>
@@ -94,112 +95,138 @@ export default {
   position: relative;
   pointer-events: auto;
   &.gradient{
-    border-radius: 40px; // 半透明边框圆角
-    padding: 5px; // 淡黄色边框宽度
-    border: 5px solid transparent; // 半透明边框宽度
+    border-radius: 64px;
+    border: 4px solid transparent;
     -webkit-background-clip: content-box, padding-box, border-box;
-    -webkit-background-origin: content-box, padding-box, border-box;
-    background-image: linear-gradient(180deg, #E783C6, #FFBEAB), -webkit-linear-gradient(#FFFCEF, #FFFCEF), -webkit-linear-gradient(rgba(255, 255, 255, 0.30), rgba(255, 255, 255, 0.30)); // 从左到右：中心背景色，内层边框色，外层边框色
-    box-shadow: inset 0 0 7px 7px #fff; // 内层阴影
+    -webkit-background-origin: content-box, padding-box ,border-box;
+    background-image: linear-gradient(180deg, #9E79EE, #9EB7FA), linear-gradient(#fff, #fff), -webkit-linear-gradient(transparent, transparent);
+    box-shadow: inset 0 0 5px 0 #fff; // 内层阴影
   }
   &.popupSize-1 {
-    width: 544px;
-    min-height: 564px;
-    border-image-source: url('@/pages/celebration/assets/tk_2.png');
-    $sliceTop: 155; // 顶部切割位置
-    $sliceBottom: 106; // 底部切割位置
+    width: 600px;
+    min-height: 480px;
+    border-image-source: url('@/pages/childrenDay/assets/tk_bg_1.png');
+    $sliceTop: 90; // 顶部切割位置
+    $sliceBottom: 188; // 底部切割位置
     border-image-slice: $sliceTop 0 $sliceBottom 0 fill;
     border-image-width: #{$sliceTop}px 0 #{$sliceBottom}px 0;
-    background: url('@/pages/celebration/assets/tk_2_1.png') no-repeat center #{$sliceTop - 4}px/544px calc(100% - #{$sliceTop}px - #{$sliceBottom}px + 8px); // 解决部分安卓机上border-image切割后出现缝隙
+    background: url('@/pages/childrenDay/assets/tk_bg_1_1.png') no-repeat center #{$sliceTop - 4}px/600px calc(100% - #{$sliceTop}px - #{$sliceBottom}px + 8px); // 解决部分安卓机上border-image切割后出现缝隙
+    .leftIcon {
+      left: -116px;
+    }
+    .rightIcon {
+      right: -116px;
+    }
   }
   &.popupSize-2 {
-    width: 632px;
-    min-height: 604px;
-    border-image-source: url('@/pages/celebration/assets/tk_1.png');
-    $sliceTop: 179; // 顶部切割位置
-    $sliceBottom: 122; // 底部切割位置
+    width: 600px;
+    min-height: 480px;
+    border-image-source: url('@/pages/childrenDay/assets/tk_bg_1.png');
+    $sliceTop: 90; // 顶部切割位置
+    $sliceBottom: 188; // 底部切割位置
     border-image-slice: $sliceTop 0 $sliceBottom 0 fill;
     border-image-width: #{$sliceTop}px 0 #{$sliceBottom}px 0;
-    background: url('@/pages/celebration/assets/tk_1_1.png') no-repeat center #{$sliceTop - 4}px/632px calc(100% - #{$sliceTop}px - #{$sliceBottom}px + 8px); // 解决部分安卓机上border-image切割后出现缝隙
+    background: url('@/pages/childrenDay/assets/tk_bg_1_1.png') no-repeat center #{$sliceTop - 4}px/600px calc(100% - #{$sliceTop}px - #{$sliceBottom}px + 8px); // 解决部分安卓机上border-image切割后出现缝隙
   }
-  &.size_1{
-    width: 580px;
-    height: 248px;
-    min-height: auto;
-    padding: 46px 0;
-  }
-  &.popupSize-empty{
-    border-image-source: none;
-    border-image-slice: 0;
-    padding: 0;
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    pointer-events: none;
+  &.popupSize-2_min {
+    @extend .popupSize-2;
+    min-height: 460px;
     .main {
-      position: relative;
-      z-index: 3;
-      width: 100%;
-      max-height: 100%;
-      min-height: 100%;
-      display: flex;
-      pointer-events: none;
-      margin: 0;
-      ::v-deep >div{
-        pointer-events: auto;
-      }
+      margin-top: 120px;
+    }
+  }
+  &.popupSize-3 {
+    width: 600px;
+    height: 300px;
+    background: url('@/pages/childrenDay/assets/tk_bg_2.png') no-repeat left top/100% 100%;
+    .main {
+      margin: 63px 0 0;
     }
   }
   .titleIcon {
-    z-index: 4;
-    width: auto;
-    height: 209px;
-    pointer-events: none;
-    position: absolute;
+    z-index: 2;
+    top: -40px;
     left: 50%;
     transform: translateX(-50%);
-    top: -97px;
+    width: 501px;
+    height: 96px;
+    background: url('@/pages/childrenDay/assets/title_1.png') no-repeat left top/100% 100%;
+    .titleText {
+      @font-face {
+        font-family: "zywyh";
+        src: url("@/pages/childrenDay/assets/zt_zywyh.ttf");
+        font-weight: normal;
+        font-style: normal;
+      }
+      font-family: zywyh;
+      font-weight: 400;
+      font-size: 40px;
+      letter-spacing: 4px;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      //position: absolute;
+      //left: 50%;
+      //top: 50%;
+      //transform: translateX(-50%);
+      > div {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translateX(-50%) translateY(-50%);
+        &:nth-child(1) {
+          color: #FFF3DE;
+          text-shadow:
+            1px 0 #C14D48, -1px 0 #C14D48, 0 1px #C14D48, 0 -1px #C14D48,
+            1px 1px #C14D48, -1px -1px #C14D48, 1px -1px #C14D48, -1px 1px #C14D48,
+            2px 0 #C14D48, -2px 0 #C14D48, 0 2px #C14D48, 0 -2px #C14D48,
+            2px 2px #C14D48, -2px -2px #C14D48, 2px -2px #C14D48, -2px 2px #C14D48,
+            3px 0 #C14D48, -3px 0 #C14D48, 0 3px #C14D48, 0 -3px #C14D48,
+            3px 3px #C14D48, -3px -3px #C14D48, 3px -3px #C14D48, -3px 3px #C14D48,
+            4px 0 #C14D48, -4px 0 #C14D48, 0 4px #C14D48, 0 -4px #C14D48,
+            4px 4px #C14D48, -4px -4px #C14D48, 4px -4px #C14D48, -4px 4px #C14D48;
+        }
+        &:nth-child(2) {
+          background: linear-gradient(180deg, #FFF3DE 0%, #FFF3DE 50%, #FFF3DE 100%);
+          background-clip: text;
+          -webkit-background-clip: text;
+          color: transparent;
+          -webkit-text-fill-color: transparent;
+        }
+      }
+    }
   }
   .main {
     position: relative;
     z-index: 3;
-    min-height: 200px;
-    max-height: 930px;
-    margin: 124px 0 80px;
+    min-height: 137px;
+    max-height: 950px;
+    margin: 70px 0 40px;
   }
   .leftIcon {
     z-index: 2;
-    width: 158px;
-    height: 518px;
-    left: -53px;
-    top: -73px;
+    width: 156px;
+    height: 359px;
+    left: -70px;
+    top: -42px;
+    //background: url('@/pages/childrenDay/assets/tk_3.png') no-repeat left top/100% 100%;
   }
   .rightIcon {
     z-index: 2;
-    width: 210px;
-    height: 565px;
-    right: -38px;
-    top: -91px;
+    width: 137px;
+    height: 441px;
+    right: -70px;
+    top: -53px;
+    //background: url('@/pages/childrenDay/assets/tk_3_1.png') no-repeat left top/100% 100%;
   }
 }
 .topLight{
   z-index: -2;
   width: 750px;
-  height: 948px;
-  background: url('@/pages/valentineDay/assets/tk_24.png') no-repeat left top/100% 100%;
+  height: 1086px;
+  //background: url('@/pages/childrenDay/assets/tk_21.png') no-repeat left top/100% 100%;
   position: fixed;
   top: 0;
-  left: 0;
-  pointer-events: none;
-}
-.bottomLight{
-  z-index: -2;
-  width: 750px;
-  height: 660px;
-  background: url('@/pages/valentineDay/assets/tk_25.png') no-repeat left top/100% 100%;
-  position: fixed;
-  bottom: 0;
   left: 0;
   pointer-events: none;
 }
