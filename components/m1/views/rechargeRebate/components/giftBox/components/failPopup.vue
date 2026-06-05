92f2ad6b0451e51d7ac92485de371cdd05afd1e2
chenweilin0106 <tr13535155jin@163.com>
2026-06-05 16:57:15 +0800

    fix(m1): 调整礼盒次数不足弹窗

--- components/m1/views/rechargeRebate/components/giftBox/components/failPopup.vue
+++ components/m1/views/rechargeRebate/components/giftBox/components/failPopup.vue
@@ -1,7 +1,7 @@
 <template>
-  <PopupBox @clickClose="clickClose">
-    <div class="desc text-center margin-row-center">开启{{config}}不足，请前往充值</div>
-    <div class="buttons flex align-center justify-center">
+  <PopupBox size="3" @clickClose="clickClose">
+    <div class="desc">开启次数不足，请前往充值</div>
+    <div class="buttons">
       <div class="cancel status0" @click="clickClose">取消</div>
       <div class="confirm status1" @click="confirm">立即前往</div>
     </div>
@@ -13,7 +13,7 @@ export default {
   props: ['config'],
   methods: {
     confirm() {
-      this.$emit('recharge')
+      this.$emit('toRecharge')
       this.clickClose()
     },
     clickClose() {
@@ -24,35 +24,44 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.desc{
-  margin-bottom: 45px;
-  font-size: 32px;
-  color: #6C4C68;
-}
-.buttons{
-  >div {
-    width: 246px;
-    height: 78px;
-    font-size: 32px;
-    color: #FFFFFF;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    line-height: 1;
-    white-space: nowrap;
-    // 渐变
-    border: 3px solid transparent;
-    border-radius: 99999px;
-    -webkit-background-clip: padding-box, border-box;
-    -webkit-background-origin: padding-box, border-box;
-    &.status0{
-      color: #4D7DDD;
-      background-image: linear-gradient(0deg, #CAE7FF, #FFFFFF), -webkit-linear-gradient(#fff, #fff);
+::v-deep .popupContent {
+  .main {
+    .desc {
+      text-align: center;
+      margin-bottom: 65px;
+      font-size: 32px;
+      line-height: 1;
+      color: #ffffff;
     }
-    &.status1{
-      margin-left: 18px;
-      color: #A9792C;
-      background-image: linear-gradient(0deg, #FFF5B0, #FFFFFF), -webkit-linear-gradient(#fff, #fff);
+    .buttons {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      >div {
+        width: 246px;
+        height: 78px;
+        font-size: 32px;
+        color: #FFFFFF;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        line-height: 1;
+        white-space: nowrap;
+        // 渐变
+        border: 3px solid transparent;
+        border-radius: 99999px;
+        -webkit-background-clip: padding-box, border-box;
+        -webkit-background-origin: padding-box, border-box;
+        &.status0 {
+          color: #4D7DDD;
+          background-image: linear-gradient(0deg, #CAE7FF, #FFFFFF), -webkit-linear-gradient(#fff, #fff);
+        }
+        &.status1 {
+          margin-left: 18px;
+          color: #A9792C;
+          background-image: linear-gradient(0deg, #FFF5B0, #FFFFFF), -webkit-linear-gradient(#fff, #fff);
+        }
+      }
     }
   }
 }
